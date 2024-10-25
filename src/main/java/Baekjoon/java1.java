ced4653fe02bdffd21ed543cbe93a13a063f0466
I-migi <sunnn10189@gmail.com>
2024-10-26 00:08:39 +0900

    init

---
+++ src/main/java/Baekjoon/java1.java
@@ -0,0 +1,11 @@
+// package Baekjoon;
+//
+// import java.util.Scanner;
+//
+// public class java1 {
+// 	public static void main(String[] args) {
+//
+// 		String input = sc.nextLine();
+// 		System.out.println(input);
+// 	}
+// }
