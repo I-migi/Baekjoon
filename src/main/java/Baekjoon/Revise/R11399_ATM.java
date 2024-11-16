3548c3a59b1d69219e915f114ffd7208ad7892ab
I-migi <sunnn10189@gmail.com>
2024-11-17 00:27:53 +0900

    .

---
+++ src/main/java/Baekjoon/Revise/R11399_ATM.java
@@ -0,0 +1,36 @@
+package Baekjoon.Revise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class R11399_ATM {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+
+		for (int i =0; i< N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		int[] sumArr = new int[N];
+		sumArr[0] = arr[0];
+		for (int i =1; i<N; i++) {
+			sumArr[i] = sumArr[i-1] + arr[i];
+		}
+		int sum = 0;
+
+		for (int i =0; i<N; i++) {
+			sum = sum + sumArr[i];
+		}
+		System.out.println(sum);
+	}
+}
---
+++ src/main/java/Baekjoon/Revise/R1260_DFS와_BFS.java
@@ -0,0 +1,69 @@
+package Baekjoon.Revise;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class R1260_DFS와_BFS {
+	static boolean visited[];
+	static ArrayList<Integer>[] A;
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int V = sc.nextInt();
+
+		A = new ArrayList[N+1];
+
+		for (int i =1; i<=N; i++) {
+			A[i] = new ArrayList<>();
+		}
+
+		for(int i = 1; i<=M; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			A[a].add(b);
+			A[b].add(a);
+		}
+
+		for (int i = 1; i<=N; i++) {
+			Collections.sort(A[i]);
+		}
+
+		visited = new boolean[N+1];
+		DFS(V);
+		System.out.println();
+		visited = new boolean[N+1];
+		BFS(V);
+	}
+	private static void DFS(int V) {
+		visited[V] = true;
+		System.out.print(V + " ");
+		for (int i : A[V]) {
+			if (!visited[i]) {
+				DFS(i);
+			}
+		}
+	}
+
+	private static void BFS(int V) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(V);
+		visited[V] = true;
+
+		while (!queue.isEmpty()) {
+			int currentV = queue.poll();
+			System.out.print(currentV + " ");
+
+			for (int i : A[currentV]) {
+				if (!visited[i]) {
+					visited[i] = true;
+					queue.add(i);
+				}
+			}
+		}
+	}
+}
