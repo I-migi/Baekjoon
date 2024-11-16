c25bfb68d0ab769134f34a7dc2fc9ced1c8e1561
I-migi <sunnn10189@gmail.com>
2024-11-16 20:08:56 +0900

    .

---
+++ src/main/java/Baekjoon/R1940_주몽.java
@@ -0,0 +1,36 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class R1940_주몽 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		int count = 0;
+		for (int i = 0; i < N-1; i++) {
+			for (int j = i+1; j < N; j++) {
+				if (arr[i] + arr[j] == M)
+					count ++;
+			}
+		}
+		System.out.println(count);
+	}
+}
---
+++ src/main/java/Baekjoon/R2164_카드2.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+public class R2164_카드2 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 1; i <=N; i++) {
+			queue.add(i);
+		}
+
+		while (queue.size() != 1) {
+			queue.poll();
+			queue.add(queue.poll());
+		}
+
+		Integer answer = queue.peek();
+		System.out.println(answer);
+
+	}
+}
